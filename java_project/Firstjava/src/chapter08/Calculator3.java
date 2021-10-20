1949b64aafde31d7ed5b91e5b28a2d76f85589b7
정지원 <chuckled@naver.com>
2021-10-20 14:04:53 +0900

    Update Calculator3.java

--- java_project/Firstjava/src/chapter08/Calculator3.java
+++ java_project/Firstjava/src/chapter08/Calculator3.java
@@ -6,33 +6,31 @@ public class Calculator3 implements Calculator{
 	int n1;
 	int n2;
 	
-	public Calculator2 (int n1, int n2) {
+	public Calculator3() {}
+	
+	public Calculator3 (int n1, int n2) {
 		this.n1 = n1;
 		this.n2 = n2;
 	}
 	
 	@Override
 	public long add(long n1, long n2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return n1+n2;
 	}
 
 	@Override
 	public long substract(long n1, long n2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return n1-n2;
 	}
 
 	@Override
 	public long multiply(long n1, long n2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return n1*n2;
 	}
 
 	@Override
 	public double divide(double n1, double n2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return n1/n2;
 	}
 
 }
