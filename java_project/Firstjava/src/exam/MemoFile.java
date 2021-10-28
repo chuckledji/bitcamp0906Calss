ed7bac0fa4f015b49878ff1f76212ef746f4bb62
정지원 <chuckled@naver.com>
2021-10-28 18:48:17 +0900

    1028

--- java_project/Firstjava/src/exam/MemoFile.java
+++ java_project/Firstjava/src/exam/MemoFile.java
@@ -2,6 +2,8 @@ package exam;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -19,17 +21,10 @@ public class MemoFile {
 	final static Scanner scanner = new Scanner(System.in);
 
 	
-	
-	//TEST
-	public static void main(String[] args) throws IOException {
-		writeMemo();
-	}
-	
-	
 	//1.메모목록을 출력하는 메소드
 	public static void printMemoList() {
 		
-		if (memoList.size()==0) {					//메모가 등록되지 않았을 경우
+		if (memoList.size()==0) {						//메모가 등록되지 않았을 경우
 			System.out.println("아직 등록된 메모가 존재하지 않습니다.");
 		}else {
 			for(int i=0; i<memoList.size(); i++) {
@@ -43,17 +38,18 @@ public class MemoFile {
 	//2-1.선택한번호의 메모장이름을 불러와 그 메모장 파일을 출력하는 메소드
 	public static void findMemoName() throws IOException {
 		
-		if(memoList.size()==0) {					//메모가 등록되지 않았을 경우
+		if(memoList.size()==0) {						//메모가 등록되지 않았을 경우
 			System.out.println("아직 등록된 메모가 존재하지 않습니다.");
 		}else {
+			printMemoList();
 			System.out.println("볼 메모장의 번호를 선택하세요.> ");
 			int memoNum = Integer.parseInt(scanner.nextLine());
-			//선택한 번호에 맞는 메모장이름을 불러온다.
-			for(int i=0; i<memoList.size(); i++) {
+			
+			for(int i=0; i<memoList.size(); i++) {		//선택한 번호에 맞는 메모장이름을 불러온다.
 				if(memoNum == i+1) {
 					String fileName = memoList.get(i);
 					System.out.println(fileName);
-					//readFile(fileName);
+					readFile(fileName);					//2-2파일출력메소드실행
 					break;
 				}else {
 					System.out.println("자료가 존재하지 않습니다.");
@@ -93,9 +89,9 @@ public class MemoFile {
 		System.out.println("메모내용을 입력해주세요. >");		//메모내용등록
 		String content = scanner.nextLine();
 
-				
+		String fileName = String.valueOf(strDate + " " + title + ".txt");		
 		//파일생성 - 파일의 이름은 날짜와 메모제목이다.(문자기반스트림 이용)
-		BufferedWriter out = new BufferedWriter(new FileWriter(strDate +" " + title + ".txt"));
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File("c:\\ExamMemo",fileName)));
 		
 		out.newLine();
 		out.write("작성날짜 : " + strDate);					//파일에 입력한 내용 저장
@@ -106,13 +102,14 @@ public class MemoFile {
 		
 		out.close();
 		System.out.println("입력이 완료 되었습니다.");
-										
-		String fileName = String.valueOf(strDate + " " + title + ".txt");
 		System.out.println(fileName+" 파일 생성");
 		
 		//리스트에 메모파일 이름을 저장한다.
 		memoList.add(fileName);
 		
-		
+	
 	}
+	
+	
+	
 }
