6464074b92da03f05985444b3594702af42dff97
정지원 <chuckled@naver.com>
2021-11-01 11:20:42 +0900

    Update 2021.10.29_exam.sql

--- sql/2021.10.29_exam.sql
+++ sql/2021.10.29_exam.sql
@@ -15,7 +15,7 @@ from emp
 
 --20. 올해 며칠이 지났는지 출력하시오. 
 --    현재날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여 데이터 형을 일치 시키시오.​
-select floor(sysdate - to_date('2021-01-01', 'YYYY.MM.DD'))
+select sysdate - to_date('2021-01-01', 'YYYY.MM.DD')
 from dual;
 
 --21. 사원들의 상관 사번을 출력하되 상관이 없는 사원에 대해서는 NULL 값 대신 0으로 출력하시오.
@@ -42,53 +42,58 @@ from emp;
 ----24. 각 담당 업무 유형별로 
 --      급여 최고액, 최저액, 총액 및 평균 액을 출력하시오. 
 --      평균에 대해서는 정수로 반올림 하시오.
-select job, max(sal) as "급여최고액", min(sal) as "급여최저액", sum(sal) as "급여총액", round(avg(sal)) as "평균급여"
+select job, max(sal)"급여최고액", min(sal)"급여최저액", sum(sal)"급여총액", round(avg(sal))"평균급여"
 from emp
 group by job;
 
 --25. COUNT(*) 함수를 이용하여 담당업무가 동일한 사원 수를 출력하시오.
-select job, count(job)"사원수"
+select job, count(*)"사원수"
 from emp
 group by job;
 
 --26. 관리자 수를 출력하시오.
-select count(job) "관리자 명수"
-from emp
-where job = 'MANAGER';
+select count(distinct (mgr)) "관리자 명수"
+from emp;
 
 --27. 급여 최고액, 급여 최저액의 차액을 출력하시오.
 select max(sal) - min( sal)"최고,최저 차액"
 from emp;
 
---28. 직급별 사원의 최저 급여를 출력하시오. 
---    관리자를 알 수 없는 사원과 최저 급여가 2000 미만인 그룹은 제외시키고 
+--28. 직급별 사원의 최저 급여를 출력하시오.       goup by job
+--    관리자를 알 수 없는 사원과                where mgr is not null
+--    최저 급여가 2000 미만인 그룹은 제외시키고  (groupby의 having으로)
 --    결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
 select job, min(sal)"최저급여"
 from emp
 where mgr is not null
 group by job
-having min(sal)>2000
+having min(sal)>=2000   --having not min(sal)<2000
 order by min(sal) desc
 ;
 
---29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오.
+--29. 각 부서에 대해 group by deptno
+--    부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오.
 --    평균 급여는 소수점 둘째 자리로 반올림 하시오.
-select deptno, count(deptno)"사원수", round(avg(sal),2)"평균급여"
+select deptno, count(*)"사원수", round(avg(sal),2)"평균급여"
 from emp
 group by deptno;
 
---30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오.
---    평균 급여는 정수로 반올림 하시오. DECODE 사용.
+--30. 각 부서에 대해 group by deptno
+--    부서번호 
+--    이름, 지역 명, 
+--    사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오.
+--    평균 급여는 정수로 반올림 하시오. 
+--    DECODE 사용.
 select deptno, 
-       decode(deptno, 10, '회계',
-                      20, '인사',
-                      30, '영업',
-                      40, '개발' ) as 부서이름,
-       decode(deptno, 10, '서울',
-                      20, '인천',
-                      30, '청주',
-                      40, '양평' ) as 지역명,
-       count(deptno)사원수,
+       decode(deptno, 10, 'ACCOUNTING',
+                      20, 'RESEARCH',
+                      30, 'SALES',
+                      40, 'OPERATIONS' ) as 부서이름,
+       decode(deptno, 10, 'NEW YORK',
+                      20, 'DALLAS',
+                      30, 'CHICAGO',
+                      40, 'BOSTON' ) as 지역명,
+       count(*)사원수,
        round(avg(sal))평균급여
 from emp
 group by deptno
@@ -97,25 +102,13 @@ group by deptno
 --31. 업무를 표시한 다음 해당 업무에 대해 부서 번호별 급여 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오. 
 --    별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. 
 --    ( hint. Decode, group by )
-select job,
-       deptno,
-       sum(decode(deptno, 10, sal,
-                  null)) as "부서10",
-       sum(decode(deptno, 20, sal,
-                  null)) as "부서20",
-       sum(decode(deptno, 30, sal,
-                  null)) as "부서30",
+select job, deptno,
+       nvl(decode(deptno, 10, sum(sal)),0) as "부서10",
+       nvl(decode(deptno, 20, sum(sal)),0) as "부서20",
+       nvl(decode(deptno, 30, sum(sal)),0) as "부서30",
        sum(sal) as "총액"
 from emp
 group by job, deptno
-order by deptno
+order by deptno, job
 ;
-
-select job, deptno, sum(decode(deptno, 10, sal, null)) "부서10 급여총액",
-                    sum(decode(deptno, 20, sal, null)) "부서20 급여총액",
-                    sum(decode(deptno, 30, sal, null)) "부서30 급여총액",
-                    sum(sal) "총액"
-from emp
-group by job, deptno
-;
-            
+      
