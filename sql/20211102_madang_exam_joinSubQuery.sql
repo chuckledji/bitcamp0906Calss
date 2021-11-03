d08456bfc2f402e8d166d171903ced31cfeeee10
정지원 <chuckled@naver.com>
2021-11-03 18:30:38 +0900

    1103

--- sql/20211102_madang_exam_joinSubQuery.sql
+++ sql/20211102_madang_exam_joinSubQuery.sql
@@ -27,8 +27,103 @@ where o.custid = c.custid and b.bookid=o.bookid and c.name ='박지성';
 
 -- (6) 박지성이 구매한 도서의 
 --  이름, 가격, 정가와 판매가격의 차이
-select bookname, price, price-saleprice as pricegap
-from book
+select b.bookname, b.price, b.price-o.saleprice as pricegap
+from book b, orders o, customer c
+where c.name = '박지성' and c.custid=o.custid and b.bookid = o.bookid
+;
+
+select b.bookname, b.price, b.price-o.saleprice as pricegap
+from book b, orders o
+where b.bookid = o.bookid and o.custid = (select custid from customer where name='박지성');
+
+
+-- (7) 박지성이 구매하지 않은 도서의 이름
+-- 도서의 이름 출력
+-- 박지성이 구매한 도서의 id
+select bookid 
+from orders o, customer c
+where o.custid = c.custid and c.name ='박지성';
+
+select bookname from book
+where bookid not in (select bookid 
+                     from orders o, customer c
+                     where o.custid = c.custid and c.name ='박지성');
+
+
+
+-- 2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
+
+-- (8) 주문하지 않은 고객의 이름(부속질의사용)
+select name from customer where custid not in (select custid from orders);
+
+select name
+from orders o, customer c
+where o.custid(+)=c.custid   --나타내려고하는 쪽의 반대쪽
+      and o.orderid is null; --주문아이디가 빈
+
+
+--(9) 주문금액의 총액과 주문의 평균금액
+select sum(saleprice), avg(saleprice)
+from orders;
+
+
+--(10) 고객의 이름과 고객별 구매액
+select name, sum(saleprice)
+from orders o, customer c
+where c.custid = o.custid
+group by name;
+
+
+-- (11) 고객의 이름과 고객이 구매한도서 목록
+select name, bookname
+from orders o, customer c, book b
+where c.custid = o.custid and o.bookid = b.bookid
+order by name
+;
+
+-- (12) 도서의가격(Book 테이블)과 판매가격(Orders 테이블)의 차이가 가장 많은 주문번호
+--차이가 가장 많은 가격
+select max(b.price-o.saleprice)
+from book b, orders o
+where b.bookid = o.bookid;
+
+select o.orderid, b.price-o.saleprice
+from book b, orders o
+where b.bookid = o.bookid and
+      b.price-o.saleprice = (select max(b.price-o.saleprice)
+                             from book b, orders o
+                             where b.bookid = o.bookid);
+
+
+-- (13) 도서의 판매액 평균 보다 
+--      자신의 구매액 평균이 더 높은 고객의 이름
+select avg(saleprice) from orders;
+
+select avg(sum(saleprice)
+from orders o, customer c
+where c.custid = o.custid;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
